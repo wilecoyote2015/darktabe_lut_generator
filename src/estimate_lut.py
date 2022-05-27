bf6b38e69662464fffad592ce4d550800dd41676
bjoern <wilecoyote2015@gmail.com>
2022-05-27 19:09:40 +0200

    refactor and fit differences to identity lut

--- src/estimate_lut.py
+++ src/estimate_lut.py
@@ -2,9 +2,56 @@ import numpy as np
 import cv2
 from PIL import ImageFilter, Image
 from scipy import optimize, interpolate, stats
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 import logging
 from tqdm import tqdm
+
+
+def align_images_ecc(im1, im2):
+    """Align image 1 to image 2.
+    From https://learnopencv.com/image-alignment-ecc-in-opencv-c-python/"""
+    # Convert images to grayscale
+    im1_gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    # Define the motion model
+    warp_mode = cv2.MOTION_HOMOGRAPHY
+
+    # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+    if warp_mode == cv2.MOTION_HOMOGRAPHY:
+        warp_matrix = np.eye(3, 3, dtype=np.float32)
+    else:
+        warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+    # Specify the number of iterations.
+    number_of_iterations = 5000
+
+    # Specify the threshold of the increment
+    # in the correlation coefficient between two iterations
+    termination_eps = 1e-5
+
+    # Define termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations, termination_eps)
+
+    # Run the ECC algorithm. The results are stored in warp_matrix.
+    (cc, warp_matrix) = cv2.findTransformECC(im2_gray, im1_gray, warp_matrix, warp_mode, criteria)
+
+    mask_ones = np.full_like(im1[..., 0], 255)
+
+    if warp_mode == cv2.MOTION_HOMOGRAPHY:
+        # Use warpPerspective for Homography
+        im1_aligned = cv2.warpPerspective(im1, warp_matrix, (im2.shape[1], im2.shape[0]),
+                                          flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        mask = cv2.warpPerspective(mask_ones, warp_matrix, (im2.shape[1], im2.shape[0]),
+                                   flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else:
+        # Use warpAffine for Translation, Euclidean and Affine
+        im1_aligned = cv2.warpAffine(im1, warp_matrix, (im2.shape[1], im2.shape[0]),
+                                     flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        mask = cv2.warpAffine(mask_ones, warp_matrix, (im2.shape[1], im2.shape[0]),
+                              flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+
+    return im1_aligned, mask
 
 
 def align_images(im1, im2):
@@ -12,7 +59,7 @@ def align_images(im1, im2):
     Align image 1 to image 2
     """
     MAX_FEATURES = 500
-    GOOD_MATCH_PERCENT = 0.15
+    GOOD_MATCH_PERCENT = 0.2
 
     # Convert images to grayscale
     im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
@@ -93,48 +140,63 @@ def apply_lut(images: [Image], lut: np.ndarray) -> [np.ndarray]:
     # raise NotImplementedError
 
 
-def estimate_lut(filepaths_images: [[str, str]], size=8) -> np.ndarray:
+def get_aligned_image_pair(path_reference, path_raw):
+    # references = []
+    # raws = []
+    # masks = []
+    # for path_reference, path_raw in tqdm(filepaths_images):
+    reference = Image.open(path_reference)
+    raw = Image.open(path_raw)
+    # reference = cv2.imread(path_reference, cv2.COLOR_BGR2RGB)
+    # raw = cv2.imread(path_raw, cv2.COLOR_BGR2RGB)
+
+    # align the images
+    print(f'aligning image {path_reference}')
+    reference_aligned, mask = align_images_ecc(
+        np.asarray(reference),
+        np.asarray(raw)
+    )
+    print('Finished alignment')
+
+    # blur the images a bit to reduce effects of spatial processing of in camera image (sharpening etc.)
+    # size_blur = (1, 1)
+    # reference_aligned_blurred = cv2.blur(reference_aligned, size_blur)
+    # raw_blurred = cv2.blur(np.asarray(raw), size_blur)
+    # mask_blurred = cv2.blur(mask, size_blur)
+
+    # reference_result = Image.fromarray(reference_aligned, mode='RGB')
+    # raw_result = Image.fromarray(np.asarray(raw), mode='RGB')
+    mask_result = mask == 255
+
+    # Image.blend(references[-1], raws[-1], 0.5).show()
+
+    return reference_aligned, np.asarray(raw), mask_result
+
+
+def estimate_lut(filepaths_images: [[str, str]], size=8, n_pixels_sample=100000) -> np.ndarray:
     """
 
     :param filepaths_images: paths of image pairs: [reference, vanilla raw development]
     :return:
     """
-    references = []
-    raws = []
-    masks = []
-
-    # open the images
     logging.info('Opening and aligning images')
+    pixels_raws = []
+    pixels_references = []
     for path_reference, path_raw in tqdm(filepaths_images):
-        reference = Image.open(path_reference)
-        raw = Image.open(path_raw)
-        # reference = cv2.imread(path_reference, cv2.COLOR_BGR2RGB)
-        # raw = cv2.imread(path_raw, cv2.COLOR_BGR2RGB)
-
-        # align the images
-        print(f'aligning image {path_reference}')
-        reference_aligned, mask = align_images(
-            np.asarray(reference),
-            np.asarray(raw)
+        pixels_reference, pixels_raw = get_pixels_sample_image_pair(
+            path_reference,
+            path_raw,
+            int(n_pixels_sample / len(filepaths_images))
         )
-        print('Finished alignment')
+        pixels_raws.append(pixels_raw)
+        pixels_references.append(pixels_reference)
 
-        # blur the images a bit to reduce effects of spatial processing of in camera image (sharpening etc.)
-        # TODO: how are nans in borders handled?
-        size_blur = (5, 5)
-        reference_aligned_blurred = cv2.blur(reference_aligned, size_blur)
-        raw_blurred = cv2.blur(np.asarray(raw), size_blur)
-        mask_blurred = cv2.blur(mask, size_blur)
+    pixels_raws = np.concatenate(pixels_raws, axis=0)
+    pixels_references = np.concatenate(pixels_references, axis=0)
 
-        references.append(Image.fromarray(reference_aligned_blurred, mode='RGB'))
-        raws.append(Image.fromarray(raw_blurred, mode='RGB'))
-        masks.append(mask_blurred == 255)
+    lut_result = perform_estimation_linear_regression(pixels_references, pixels_raws, size)
 
-    # references[0].show()
-
-    perform_estimation_linear_regression(references, raws, masks, size)
-
-    return params_lut_optimized
+    return lut_result
 
 
 def perform_estimation_local_mean(references: [Image], raws: [Image], masks: [np.ndarray], size):
@@ -186,7 +248,7 @@ def perform_estimation_local_mean(references: [Image], raws: [Image], masks: [np
     return result
 
 
-def sample_indices_pixels(pixels, n_samples=1000, uniform=False):
+def sample_indices_pixels(pixels, n_samples=100000, uniform=False):
     if uniform:
         histogram, edges = np.histogramdd(pixels, density=False,
                                           bins=np.tile(np.linspace(0, 256, 10)[np.newaxis, ...], [3, 1]))
@@ -210,32 +272,34 @@ def sample_indices_pixels(pixels, n_samples=1000, uniform=False):
     return indices_sampled
 
 
-def perform_estimation_linear_regression(references: [Image], raws: [Image], masks: [np.ndarray], size):
-    references_arrays = [np.asarray(img) for img in references]
-    raws_arrays = [np.asarray(img) for img in raws]
+def get_pixels_sample_image_pair(path_reference, path_raw, n_samples):
+    reference, raw, mask = get_aligned_image_pair(path_reference, path_raw)
 
+    pixels_reference = np.reshape(
+        reference,
+        (
+            reference.shape[0] * reference.shape[1],
+            reference.shape[-1]
+        )
+    )[np.reshape(mask, mask.shape[0] * mask.shape[1])]
+    pixels_raw = np.reshape(
+        raw,
+        (
+            raw.shape[0] * raw.shape[1],
+            raw.shape[-1]
+        )
+    )[np.reshape(mask, mask.shape[0] * mask.shape[1])]
+
+    indices_sample = sample_indices_pixels(pixels_raw)
+    result_raw = pixels_raw[indices_sample].astype(np.float64)
+    result_reference = pixels_reference[indices_sample].astype(np.float64)
+
+    return result_reference, result_raw
+
+
+def perform_estimation_linear_regression(pixels_references, pixels_raws, size):
     coordinates = np.linspace(0, 255, size)
-
     step_size = 255. / (size - 1)
-
-    # TODO: reshaping correct?
-    pixels_references = np.concatenate([
-        np.reshape(img, (img.shape[0] * img.shape[1], img.shape[-1]))[np.reshape(mask, mask.shape[0] * mask.shape[1])]
-        for img, mask in zip(references_arrays, masks)
-    ],
-        axis=0
-    )
-    pixels_raws = np.concatenate([
-        np.reshape(img, (img.shape[0] * img.shape[1], img.shape[-1]))[np.reshape(mask, mask.shape[0] * mask.shape[1])]
-        for img, mask in zip(raws_arrays, masks)
-    ],
-        axis=0
-    )
-
-    print('sampling pixels')
-    indices_sample = sample_indices_pixels(pixels_raws)
-    pixels_raws = pixels_raws[indices_sample]
-    pixels_references = pixels_references[indices_sample]
 
     # feature matrix with order of permutation: r, g, b
     print('generating design matrix')
@@ -245,12 +309,6 @@ def perform_estimation_linear_regression(references: [Image], raws: [Image], mas
     differences_channels = pixels_raws[..., np.newaxis] - np.stack([coordinates] * 3, axis=0)[np.newaxis, ...]
     differences_channels_relative_grid_steps = differences_channels / step_size
     weights_distances_channels = np.maximum(1. - np.abs(differences_channels_relative_grid_steps), 0.)
-    # differences_colors_squared = differences_colors**2
-
-    identity = make_lut_identity(size)
-    lut = get_lut_pillow(identity)
-    filtered = raws[0].filter(lut)
-    filtered.show()
 
     idx_design_matrix = 0
     for idx_r in tqdm(range(size)):
@@ -267,36 +325,46 @@ def perform_estimation_linear_regression(references: [Image], raws: [Image], mas
                 idx_design_matrix += 1
 
     print('fitting lookup table coefficients')
-    result = np.zeros(
-        (size, size, size, 3),
-        np.float
-    )
-    for idx_channel in range(3):
-        regression = LinearRegression(fit_intercept=False, positive=True)
-        regression.fit(design_matrix, pixels_references[..., idx_channel])
 
-        lut_channel = np.reshape(regression.coef_, [size, size, size])
-        result[..., idx_channel] = lut_channel
+    result = make_lut_identity(size)
+    differences_references_raw = pixels_references - pixels_raws
+    for idx_channel in range(3):
+        # regression = LinearRegression(fit_intercept=False, positive=False)
+        regression = Lasso(alpha=1e-5, fit_intercept=False, positive=False)
+        regression.fit(design_matrix, differences_references_raw[..., idx_channel])
+
+        lut_difference_channel = np.reshape(regression.coef_, [size, size, size])
+        result[..., idx_channel] += lut_difference_channel
 
     result = np.clip(result, a_min=0., a_max=255)
 
     lut = get_lut_pillow(result)
 
-    filtered = raws[0].filter(lut)
+    w_h_img_evaluation = int(np.floor(np.sqrt(pixels_references.shape[0])))
+    size_img_evaluation = w_h_img_evaluation ** 2
+
+    img_evaluation_raw = Image.fromarray(
+        pixels_raws[:size_img_evaluation].reshape([w_h_img_evaluation, w_h_img_evaluation, 3]).astype(np.uint8)
+    )
+    img_evaluation_reference = Image.fromarray(
+        pixels_references[:size_img_evaluation].reshape([w_h_img_evaluation, w_h_img_evaluation, 3]).astype(np.uint8)
+    )
+
+    transformed = img_evaluation_raw.filter(lut)
     # filtered_np = apply_lut(raws, result)[0]
 
-    references[0].show()
-    filtered.show()
+    # img_evaluation_reference.show()
+    # transformed.show()
 
-    rmse_pre = np.sqrt(np.mean((np.asarray(raws[0]) - np.asarray(references[0])) ** 2))
-    rmse_past = np.sqrt(np.mean((filtered - np.asarray(references[0])) ** 2))
-    # rmse_past_np = np.sqrt(np.mean((filtered_np - np.asarray(references[0]))**2))
+    rmse_pre = np.sqrt(np.mean((np.asarray(img_evaluation_raw, dtype=np.float32) - np.asarray(img_evaluation_reference,
+                                                                                              dtype=np.float32)) ** 2))
+    rmse_past = np.sqrt(np.mean(
+        (np.asarray(transformed, dtype=np.float32) - np.asarray(img_evaluation_reference, dtype=np.float32)) ** 2))
 
-    print(f'rmse pre: {rmse_pre}')
-    print(f'rmse past: {rmse_past}')
-    # print(f'rmse past np: {rmse_past_np}')
+    print(f'rmse without lut: {rmse_pre}')
+    print(f'rmse with fitted lut: {rmse_past}')
 
-    pass
+    return result
 
 
 def get_lut_pillow(
@@ -330,6 +398,4 @@ if __name__ == '__main__':
         )
     ]
 
-    identity = make_lut_identity(4)
-
-    estimate_lut(paths, size=8)
+    estimate_lut(paths, size=8, n_pixels_sample=1000000)
