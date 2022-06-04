0f6de04befbd707a0446df3acb69acf8985adfad
bjoern <wilecoyote2015@gmail.com>
2022-06-04 22:04:47 +0200

    update version

--- src/darktable_lut_generator/estimate_lut.py
+++ src/darktable_lut_generator/estimate_lut.py
@@ -48,6 +48,31 @@ from scipy import ndimage
 #   is there a problem when exporting the sample images regarding the rendering intent,
 #   so that out-of-gamut values mapping is not bijective?
 
+# def estimate_transform_features(im1_grey, im2_grey):
+#     import numpy as np
+#     import cv2 as cv
+#     MIN_MATCH_COUNT = 10
+#     img1 = cv.imread('box.png', 0)  # queryImage
+#     img2 = cv.imread('box_in_scene.png', 0)  # trainImage
+#     # Initiate SIFT detector
+#     sift = cv.SIFT_create()
+#     # find the keypoints and descriptors with SIFT
+#     kp1, des1 = sift.detectAndCompute(img1, None)
+#     kp2, des2 = sift.detectAndCompute(img2, None)
+#     FLANN_INDEX_KDTREE = 1
+#     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+#     search_params = dict(checks=50)
+#     flann = cv.FlannBasedMatcher(index_params, search_params)
+#     matches = flann.knnMatch(des1, des2, k=2)
+#     # store all the good matches as per Lowe's ratio test.
+#     good = []
+#     for m, n in matches:
+#         if m.distance < 0.7 * n.distance:
+#             good.append(m)
+#
+#     H, _ = cv.findHomography( features0.matched_pts, \
+#         features1.matched_pts, cv.RANSAC, 5.0)
+
 def align_images_ecc(im1, im2):
     """Align image 1 to image 2.
     From https://learnopencv.com/image-alignment-ecc-in-opencv-c-python/"""
